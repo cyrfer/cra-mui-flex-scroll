9aaaa2626d4800d02f4c77bcb2043c2089131886
Grant, John K <john.k.grant@warnerbros.com>
2019-08-22 23:16:45 -0700

    demo scrollable item in nested flex containers.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import './flex.css';
+
+
+const LoremIpsum = () => {
+  return (
+<span>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et quam hendrerit, laoreet elit vel, semper nulla. Aenean eget lacinia mauris. Nullam nec semper nulla, vel aliquam neque. Nullam dignissim efficitur velit vel imperdiet. Duis et posuere justo. Nunc id velit sem. Suspendisse sit amet enim ut tortor eleifend finibus. Aenean finibus nisi augue, at dictum purus tincidunt sit amet. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Maecenas ultricies lacus ut nisi congue dictum.
+
+Nam molestie pellentesque turpis, in mollis tellus condimentum volutpat. Sed non turpis a lorem fermentum facilisis vitae in erat. Ut ac leo ultrices, convallis sem et, dignissim augue. Aliquam bibendum ipsum maximus suscipit sollicitudin. Nulla sagittis luctus nibh, eget rutrum nulla malesuada sit amet. Maecenas consequat porta quam, vitae semper ex dapibus vitae. Aenean porttitor et purus eget imperdiet. Proin viverra orci tellus, nec egestas nibh faucibus a. Suspendisse at rhoncus tellus, sed tempor mauris.
+
+Pellentesque in finibus leo, at gravida nunc. Mauris id elit magna. Praesent et aliquam dolor. Nullam commodo tempor placerat. Vivamus sit amet leo pulvinar, consectetur odio quis, bibendum sem. Aenean nec ipsum augue. Etiam facilisis convallis mi at convallis. Maecenas tempus pellentesque urna, sit amet luctus velit euismod et. Vestibulum pellentesque mattis maximus. Pellentesque magna enim, sagittis id maximus quis, feugiat ut justo.
+
+Nullam enim risus, dignissim id pharetra et, ullamcorper tristique arcu. Duis nec ornare eros. Nulla in semper lacus, quis suscipit diam. Vivamus a lacus ut ipsum posuere pharetra ut eget arcu. Suspendisse potenti. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Quisque nec viverra augue, et molestie erat. Ut sollicitudin ante justo, vel mattis ex egestas eu.
+
+Integer nunc dui, tempus eget turpis viverra, iaculis efficitur urna. Nunc vitae consectetur sapien, at aliquam ligula. Nulla eu fringilla enim. Aliquam semper porttitor nunc, id scelerisque risus commodo sed. Mauris dignissim dui vitae tellus hendrerit, at sollicitudin erat facilisis. Phasellus consequat urna blandit nunc laoreet bibendum. Fusce tristique sodales urna, non feugiat urna porttitor sit amet. Nullam maximus condimentum lorem, vitae ornare sapien fermentum non. Nullam quam enim, vulputate et elementum nec, congue a justo. Nullam non suscipit nisi. Donec mattis tincidunt est in iaculis.
+
+Pellentesque in finibus leo, at gravida nunc. Mauris id elit magna. Praesent et aliquam dolor. Nullam commodo tempor placerat. Vivamus sit amet leo pulvinar, consectetur odio quis, bibendum sem. Aenean nec ipsum augue. Etiam facilisis convallis mi at convallis. Maecenas tempus pellentesque urna, sit amet luctus velit euismod et. Vestibulum pellentesque mattis maximus. Pellentesque magna enim, sagittis id maximus quis, feugiat ut justo.
+
+Nullam enim risus, dignissim id pharetra et, ullamcorper tristique arcu. Duis nec ornare eros. Nulla in semper lacus, quis suscipit diam. Vivamus a lacus ut ipsum posuere pharetra ut eget arcu. Suspendisse potenti. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Quisque nec viverra augue, et molestie erat. Ut sollicitudin ante justo, vel mattis ex egestas eu.
+
+Integer nunc dui, tempus eget turpis viverra, iaculis efficitur urna. Nunc vitae consectetur sapien, at aliquam ligula. Nulla eu fringilla enim. Aliquam semper porttitor nunc, id scelerisque risus commodo sed. Mauris dignissim dui vitae tellus hendrerit, at sollicitudin erat facilisis. Phasellus consequat urna blandit nunc laoreet bibendum. Fusce tristique sodales urna, non feugiat urna porttitor sit amet. Nullam maximus condimentum lorem, vitae ornare sapien fermentum non. Nullam quam enim, vulputate et elementum nec, congue a justo. Nullam non suscipit nisi. Donec mattis tincidunt est in iaculis.
+
+Pellentesque in finibus leo, at gravida nunc. Mauris id elit magna. Praesent et aliquam dolor. Nullam commodo tempor placerat. Vivamus sit amet leo pulvinar, consectetur odio quis, bibendum sem. Aenean nec ipsum augue. Etiam facilisis convallis mi at convallis. Maecenas tempus pellentesque urna, sit amet luctus velit euismod et. Vestibulum pellentesque mattis maximus. Pellentesque magna enim, sagittis id maximus quis, feugiat ut justo.
+
+Nullam enim risus, dignissim id pharetra et, ullamcorper tristique arcu. Duis nec ornare eros. Nulla in semper lacus, quis suscipit diam. Vivamus a lacus ut ipsum posuere pharetra ut eget arcu. Suspendisse potenti. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Quisque nec viverra augue, et molestie erat. Ut sollicitudin ante justo, vel mattis ex egestas eu.
+
+Integer nunc dui, tempus eget turpis viverra, iaculis efficitur urna. Nunc vitae consectetur sapien, at aliquam ligula. Nulla eu fringilla enim. Aliquam semper porttitor nunc, id scelerisque risus commodo sed. Mauris dignissim dui vitae tellus hendrerit, at sollicitudin erat facilisis. Phasellus consequat urna blandit nunc laoreet bibendum. Fusce tristique sodales urna, non feugiat urna porttitor sit amet. Nullam maximus condimentum lorem, vitae ornare sapien fermentum non. Nullam quam enim, vulputate et elementum nec, congue a justo. Nullam non suscipit nisi. Donec mattis tincidunt est in iaculis.
+
+Pellentesque in finibus leo, at gravida nunc. Mauris id elit magna. Praesent et aliquam dolor. Nullam commodo tempor placerat. Vivamus sit amet leo pulvinar, consectetur odio quis, bibendum sem. Aenean nec ipsum augue. Etiam facilisis convallis mi at convallis. Maecenas tempus pellentesque urna, sit amet luctus velit euismod et. Vestibulum pellentesque mattis maximus. Pellentesque magna enim, sagittis id maximus quis, feugiat ut justo.
+
+Nullam enim risus, dignissim id pharetra et, ullamcorper tristique arcu. Duis nec ornare eros. Nulla in semper lacus, quis suscipit diam. Vivamus a lacus ut ipsum posuere pharetra ut eget arcu. Suspendisse potenti. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Quisque nec viverra augue, et molestie erat. Ut sollicitudin ante justo, vel mattis ex egestas eu.
+
+Integer nunc dui, tempus eget turpis viverra, iaculis efficitur urna. Nunc vitae consectetur sapien, at aliquam ligula. Nulla eu fringilla enim. Aliquam semper porttitor nunc, id scelerisque risus commodo sed. Mauris dignissim dui vitae tellus hendrerit, at sollicitudin erat facilisis. Phasellus consequat urna blandit nunc laoreet bibendum. Fusce tristique sodales urna, non feugiat urna porttitor sit amet. Nullam maximus condimentum lorem, vitae ornare sapien fermentum non. Nullam quam enim, vulputate et elementum nec, congue a justo. Nullam non suscipit nisi. Donec mattis tincidunt est in iaculis.
+
+</span>
+  )
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div class="container column full-height">
+
+<div class="item" id="top">
+    top
+</div>
+
+<div class="grow container row" id="middle">
+    <div class="grow container column" id="left">
+        <div class="item" id="left-top">
+            left-top
+        </div>
+        <div class="item grow overflow" id="left-bottom">
+          <LoremIpsum />
+        </div>
     </div>
+    <div class="item grow" id="right">
+        right
+    </div>
+</div>
+
+<div class="item" id="footer">
+    footer
+</div>
+</div>
   );
 }
 
